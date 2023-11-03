959f5a848f71c71e827d24231ec5c0a5f8ff7404
Rehan Khan <rehan.khan.teo@gotoko.co.id>
2023-11-03 23:54:45 +0500

    Code cleanup

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,11 +4,8 @@ async function getData(page = 1) {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=4`
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
@@ -17,7 +14,7 @@ async function getData(page = 1) {
 
 export default async function Posts({ searchParams }: any) {
   const data = await getData(searchParams?.page);
-  console.log(searchParams);
+
   return (
     <main>
       <CustomTable postTitle="Posts" data={data} />
